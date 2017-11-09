7a44d9cdf49f6b008944aadf7cc7c50213a5c000
GiangNNgo <gnngo@uci.edu>
2017-11-09 11:54:46 -0800

    Added some comments

--- mySQLConnectorExample.py
+++ mySQLConnectorExample.py
@@ -1,3 +1,7 @@
+## This example uses mysql-connector and openpyxl to fetch data from a database 
+## and write it into an excel file.
+## Real applications will depend on the actual database's structures.
+
 from openpyxl import Workbook as WB
 import mysql.connector
 from mysql.connector import (connection)
@@ -5,17 +9,21 @@ from mysql.connector import errorcode
 
 wb = WB()
 
-## This function connects to the database and returns the 
-def connectToMySQLDatabase(hostUrl, databaseName, username, newPassword):
+## This function connects to the database and returns the cursor
+def connectToMySQLDatabase(hostUrl, databaseName, username, newPassword, newQuery):
 	print("Connecting to the database...")
 	try:
+		# First, connect to the database
 		myConnection = connection.MySQLConnection(user = username, password = newPassword, 
 			host = hostUrl, database = databaseName)
-		# Do something here
+		# Execute the query here
 		myConnection.close()
 	except mysql.connector.Error as err:
 		print("Exception detected! " + str(err.errno))
 		print(err)
 	
+def main():
+	print "Hello World!"
 
-
+if __name__== "__main__":
+	main()
