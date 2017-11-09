1647ed58fb42cbc332a889ca2ae0bf233a6bdf24
GiangNNgo <gnngo@uci.edu>
2017-11-09 11:38:12 -0800

    Changed file names

--- mySQLConnectorExample.py
+++ mySQLConnectorExample.py
@@ -5,17 +5,17 @@ from mysql.connector import errorcode
 
 wb = WB()
 
+## This function connects to the database and returns the 
 def connectToMySQLDatabase(hostUrl, databaseName, username, newPassword):
-	print("Let's connect to a database.")
+	print("Connecting to the database...")
 	try:
 		myConnection = connection.MySQLConnection(user = username, password = newPassword, 
 			host = hostUrl, database = databaseName)
+		# Do something here
 		myConnection.close()
 	except mysql.connector.Error as err:
 		print("Exception detected! " + str(err.errno))
 		print(err)
-	print("Well let's keep going!")
+	
 
 
-# 
-connectToMySQLDatabase('a', 'b', 'c', 'd')
